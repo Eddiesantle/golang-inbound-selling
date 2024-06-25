22e57dc3c37e7311147aab5c1b01818a45dedf65
Eddiesantle <wedleys8@gmail.com>
2024-06-24 22:08:28 -0300

    FEAT: integration between systems

--- cmd/events/main.go
+++ cmd/events/main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
 
 	"github.com/Eddiesantle/golang-inbound-selling/internal/events/infra/repository"
 	"github.com/Eddiesantle/golang-inbound-selling/internal/events/infra/service"
@@ -13,9 +20,9 @@ import (
 )
 
 func main() {
-	db, err := sql.Open("mysql", "test_user:test_password@tcp(localgost:3306)/test_go")
+	db, err := sql.Open("mysql", "test_user:test_password@tcp(golang-mysql:3306)/test_db")
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 	defer db.Close()
 
@@ -25,8 +32,8 @@ func main() {
 	}
 
 	partnerBaseURLs := map[int]string{
-		1: "http://localhost:9080/api1",
-		2: "http://localhost:9080/api2",
+		1: "http://host.docker.internal:8000/partner1",
+		2: "http://host.docker.internal:8000/partner2",
 	}
 
 	listEventsUseCase := usecase.NewListEventsUseCase(eventRepo)
@@ -51,38 +58,36 @@ func main() {
 	r.HandleFunc("/events/{eventID}/spots", eventsHandler.ListSpots)
 	r.HandleFunc("POST /checkout", eventsHandler.BuyTickets)
 
-	http.ListenAndServe(":8080", r)
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: r,
+	}
 
-	// server := &http.Server{
-	// 	Addr:    ":8080",
-	// 	Handler: r,
-	// }
+	// Canal para escutar sinais do sistema operacional
+	idleConnsClosed := make(chan struct{})
+	go func() {
+		sigint := make(chan os.Signal, 1)
+		signal.Notify(sigint, syscall.SIGINT, syscall.SIGTERM)
+		<-sigint
 
-	// // Canal para escutar sinais do sistema operacional
-	// idleConnsClosed := make(chan struct{})
-	// go func() {
-	// 	sigint := make(chan os.Signal, 1)
-	// 	signal.Notify(sigint, syscall.SIGINT, syscall.SIGTERM)
-	// 	<-sigint
+		// Recebido sinal de interrupção, iniciando o graceful shutdown
+		log.Println("Recebido sinal de interrupção, iniciando o graceful shutdown...")
 
-	// 	// Recebido sinal de interrupção, iniciando o graceful shutdown
-	// 	log.Println("Recebido sinal de interrupção, iniciando o graceful shutdown...")
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
 
-	// 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	// 	defer cancel()
+		if err := server.Shutdown(ctx); err != nil {
+			log.Printf("Erro no graceful shutdown: %v\n", err)
+		}
+		close(idleConnsClosed)
+	}()
 
-	// 	if err := server.Shutdown(ctx); err != nil {
-	// 		log.Printf("Erro no graceful shutdown: %v\n", err)
-	// 	}
-	// 	close(idleConnsClosed)
-	// }()
+	// Iniciando o servidor HTTP
+	log.Println("Servidor HTTP rodando na porta 8080")
+	if err := server.ListenAndServe(); err != http.ErrServerClosed {
+		log.Fatalf("Erro ao iniciar o servidor HTTP: %v\n", err)
+	}
 
-	// // Iniciando o servidor HTTP
-	// log.Println("Servidor HTTP rodando na porta 8080")
-	// if err := server.ListenAndServe(); err != http.ErrServerClosed {
-	// 	log.Fatalf("Erro ao iniciar o servidor HTTP: %v\n", err)
-	// }
-
-	// <-idleConnsClosed
-	// log.Println("Servidor HTTP finalizado")
+	<-idleConnsClosed
+	log.Println("Servidor HTTP finalizado")
 }
